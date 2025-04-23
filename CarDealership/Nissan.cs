3bc70309a9dfc433eb4a124655ac225b7dede923
Angel May <Anmaychable@gmail.com>
2025-04-23 10:00:16 -0700

    Added Toyota & Nissan Class. Toyota unique property will be mileage and Nissa unique property will be transmisson.

---
+++ CarDealership/Nissan.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarDealership
+{
+    public class Nissan : Car
+    {
+        public string Transmission { get; set; }
+
+        public Nissan(string make, string model, string color, int age, decimal price, string transmisson)
+            : base(make, model, color, age, price)
+        {
+            Transmission = transmisson;
+        }
+
+        public override string GetDisplayText()
+        {
+            return base.GetDisplayText() + $"\n{Transmission}";
+        }
+    }
+}
---
+++ CarDealership/Toyota.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarDealership
+{
+    public class Toyota : Car
+        {
+            public int Mileage { get; set; }
+
+            public Toyota(string make, string model, string color, int age, decimal price, int mileage)
+                : base(make, model, color, age, price)
+            {
+                Mileage = mileage;
+            }
+
+            public override string GetDisplayText()
+            {
+                return base.GetDisplayText() + $"\n{Mileage}";
+            }
+        }
+}
