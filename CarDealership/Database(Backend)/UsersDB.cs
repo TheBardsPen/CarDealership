fe75411759255df9bae312b8242f242249476a5d
Angel May <Anmaychable@gmail.com>
2025-05-22 09:06:37 -0700

    Organizing Databases

---
+++ CarDealership/Database(Backend)/UsersDB.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarDealership.Database_Backend_
+{
+    internal class UsersDB
+    {
+    }
+}
